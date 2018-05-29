0eea88025add66918dfe71179f19a0ce4a1fd235
gabrielsaruhashi <gabriel.saruhashi@gmail.com>
2018-05-29 12:48:51 +0700

    fixing minified production error'

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: ['./src/index.js'],
   output: {
@@ -34,5 +36,10 @@ module.exports = {
       aggregateTimeout: 300,
       poll: 1000
     }
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+  ]
 };
